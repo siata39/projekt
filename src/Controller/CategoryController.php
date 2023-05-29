d0d18b8beeb1329225e22ec58553465de51779a1
julia <you@example.com>
2023-05-29 20:57:44 +0000

    Init commit

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Category controller.
+ */
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Service\CategoryServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Type\CategoryType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+/**
+ * Class CategoryController.
+ */
+#[Route('/category')]
+class CategoryController extends AbstractController
+{
+    /**
+     * Category service.
+     */
+    private CategoryServiceInterface $categoryService;
+
+    /**
+     * Translator.
+     *
+     * @var TranslatorInterface
+     */
+    private TranslatorInterface $translator;
+
+    /**
+     * Constructor.
+     *
+     * @param CategoryServiceInterface $taskService Task service
+     * @param TranslatorInterface      $translator  Translator
+     */
+    public function __construct(CategoryServiceInterface $categoryService, TranslatorInterface $translator)
+    {
+        $this->categoryService = $categoryService;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Create action.
+     *
+     * @param Request $request HTTP request
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        '/create',
+        name: 'category_create',
+        methods: 'GET|POST',
+    )]
+    public function create(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->categoryService->save($category);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('Category created successfully!')
+            );
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render(
+            'category/create.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    /**
+     * Edit action.
+     *
+     * @param Request  $request  HTTP request
+     * @param Category $category Category entity
+     *
+     * @return Response HTTP response
+     */
+    #[Route('/{id}/edit', name: 'category_edit', requirements: ['id' => '[1-9]\d*'], methods: 'GET|PUT')]
+    public function edit(Request $request, Category $category): Response
+    {
+        $form = $this->createForm(
+            CategoryType::class,
+            $category,
+            [
+                'method' => 'PUT',
+                'action' => $this->generateUrl('category_edit', ['id' => $category->getId()]),
+            ]
+        );
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->categoryService->save($category);
+
+            $this->addFlash(
+                'success',
+                $this->translator->trans('message.created_successfully')
+            );
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render(
+            'category/edit.html.twig',
+            [
+                'form' => $form->createView(),
+                'category' => $category,
+            ]
+        );
+    }
+
+    /**
+     * Index action.
+     *
+     * @param Request $request HTTP Request
+     *
+     * @return Response HTTP response
+     */
+    #[Route(name: 'category_index', methods: 'GET')]
+    public function index(Request $request): Response
+    {
+        $pagination = $this->categoryService->getPaginatedList(
+            $request->query->getInt('page', 1)
+        );
+
+        return $this->render('category/index.html.twig', ['pagination' => $pagination]);
+    }
+
+    /**
+     * Show action.
+     *
+     * @param Category $category Category
+     *
+     * @return Response HTTP response
+     */
+    #[Route(
+        '/{id}',
+        name: 'category_show',
+        requirements: ['id' => '[1-9]\d*'],
+        methods: 'GET'
+    )]
+    public function show(Category $category): Response
+    {
+        return $this->render('category/show.html.twig', ['category' => $category]);
+    }
+}
---
+++ src/Service/CategoryServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Category service interface.
+ */
+
+namespace App\Service;
+
+use App\Entity\Category;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+
+interface CategoryServiceInterface
+{
+    /**
+     * Save entity.
+     *
+     * @param Category $category Category entity
+     */
+    public function save(Category $category): void;
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface<string, mixed> Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface;
+}
